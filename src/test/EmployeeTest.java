494a0fe1b5442c0dd950d2387dd647e0953ef2d2
Rafael <rafamont@espol.edu.ec>
2020-08-26 15:41:33 -0500

    Inicializacion

---
+++ src/test/EmployeeTest.java
@@ -0,0 +1,17 @@
+package test;
+
+import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
+
+
+import tallerpruebas.Employee;
+import tallerpruebas.EmployeeType;
+
+public class EmployeeTest {
+
+
+	
+
+}
+

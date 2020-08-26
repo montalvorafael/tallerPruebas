bcbd8f904c808c3c9fe244c566a3a4f4d5b13fdb
Rafael <rafamont@espol.edu.ec>
2020-08-26 16:00:40 -0500

    Pruebas YearBonusEuroWorker/manager/supervisor

--- src/test/EmployeeTest.java
+++ src/test/EmployeeTest.java
@@ -9,6 +9,29 @@ import tallerpruebas.Employee;
 import tallerpruebas.EmployeeType;
 
 public class EmployeeTest {
+	@Test
+	public void CalculateYearBonusWorkerEurotest() {
+		Employee employee = new Employee(450,"euro",10,EmployeeType.Worker);
+		
+		double salario= employee.CalculateYearBonus();
+		assertEquals(386.0, salario,5.0);
+	}
+	
+	@Test
+	public void CalculateYearBonusSupervisorEurotest() {
+		Employee employee = new Employee(550,"euro",10,EmployeeType.Supervisor);
+		
+		double salario= employee.CalculateYearBonus();
+		assertEquals(715.5, salario,5.0);
+	}
+	
+	@Test
+	public void CalculateYearBonusManagerEurotest() {
+		Employee employee = new Employee(750,"euro",10,EmployeeType.Manager);
+		
+		double salario= employee.CalculateYearBonus();
+		assertEquals(1098.5, salario,5.0);
+	}
 
 
 	
